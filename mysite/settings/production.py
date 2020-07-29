2dd96fc0da759017f5ece9f6df5ed404801a0980
akhsar21 <akhsarodhe@gmail.com>
2020-07-29 22:03:27 +0800

    mysite/settings/production: database

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 from .base import *
+import dj_database_url
 import os
 
 env = os.environ.copy()
@@ -10,8 +11,6 @@ DEBUG = False
 # Allow all host headers
 ALLOWED_HOSTS = ['aksar-as.herokuapp.com', '127.0.0.1']
 
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 COMPRESS_OFFLINE = True
@@ -19,6 +18,11 @@ COMPRESS_CSS_FILTERS = [
     'compressor.filters.css_default.CssAbsoluteFilter',
     'compressor.filters.cssmin.CSSMinFilter',
 ]
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
+
 COMPRESS_CSS_HASHING_METHOD = 'content'
 
 SESSION_COOKIE_SECURE = True
