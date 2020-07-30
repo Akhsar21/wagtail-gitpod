d034ecdb2379fe5656b41b52cda7b73df78777c6
Akhsar21 <akhsarodhe@gmail.com>
2020-07-30 23:01:23 +0800

    add

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -19,9 +19,9 @@ COMPRESS_CSS_FILTERS = [
     'compressor.filters.cssmin.CSSMinFilter',
 ]
 
-#DTABASES = {
-#    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-#}
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
 
 COMPRESS_CSS_HASHING_METHOD = 'content'
 
