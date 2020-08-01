3f9f143f054a67d23ad804ef5adb11eb26d65b12
akhsar21 <akhsarodhe@gmail.com>
2020-08-01 14:34:07 +0800

    subscribers/admin: subscriber

--- subscribers/admin.py
+++ subscribers/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    modeladmin_register,
+)
 
-# Register your models here.
+from .models import Subscriber
+
+
+class SubscriberAdmin(ModelAdmin):
+    """Subscriber admin"""
+
+    model = Subscriber
+    menu_label = "Subscribers"
+    menu_icon = "placeholder"
+    menu_order = 290
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ("email", "full_name",)
+    search_fields = ("email", "full_name",)
+
+
+modeladmin_register(SubscriberAdmin)
