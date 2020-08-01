7ffd11c3b904959b8fede36a51c8957ecc2b26ba
akhsar21 <akhsarodhe@gmail.com>
2020-08-01 11:39:39 +0800

    flex app

---
+++ flex/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks as streamfield_blocks
+
+from streams import blocks
+
+# Create your models here.
+
+
+class FlexPage(Page):
+    """Flexible page class."""
+
+    subpage_types = ['flex.FlexPage', 'contact.ContactPage']
+    parent_page_types = [
+        'flex.FlexPage',
+        'home.HomePage',
+    ]
+
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock()),
+            ("full_richtext", blocks.RichtextBlock()),
+            ("simple_richtext", blocks.SimpleRichtextBlock()),
+            ("cards", blocks.CardBlock()),
+            ("cta", blocks.CTABlock()),
+            ("button", blocks.ButtonBlock()),
+            ("char_block", streamfield_blocks.CharBlock(
+                required=True,
+                help_text='Oh wow this is help text!!',
+                min_length=10,
+                max_length=50,
+                template="streams/char_block.html",
+            ))
+        ], blank=True, null=True
+    )
+
+    subtitle = models.CharField(max_length=100, blank=True, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+        StreamFieldPanel("content"),
+    ]
+
+    class Meta:
+        verbose_name = "Flex Page"
+        verbose_name_plural = "Flex Pages"
--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -26,6 +26,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     'blog',
     'contact',
+    'flex',
     'home',
     'search',
     'site_settings',
